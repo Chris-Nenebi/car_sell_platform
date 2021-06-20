dc1a3af83a768d23650e6830ea067d0614c3e0ea
Springer <kwametetth33@yahoo.com>
2021-06-20 12:43:16 +0000

    pages and navigation links created

--- carzone/urls.py
+++ carzone/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("pages.urls"))
+    path("",include("pages.urls")) # initial home page which is located in the pages app
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from django.urls import path,include
 from pages import views
 
 urlpatterns = [
-    path("",views.home,name="home")
+    path("",views.home,name="home"),# this will link the home method in the view.py file
+    path("about",views.about,name="about"), # this will link the about method in the view.py file
+    path("services",views.services,name="services"),
+    path("contact",views.contact,name="contact"),
 ]
